7d438f7630f46397fe008b074305f8faef50c9ff
AwesomeLillian <106246099+AwesomeLillian@users.noreply.github.com>
2026-09-18 11:31:55 +0300

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -614,7 +614,7 @@ function Index() {
 
       <footer className="border-t border-border/60 bg-card/40">
         <div className="mx-auto flex max-w-6xl flex-col gap-6 px-5 py-10 md:flex-row md:items-center md:justify-between">
-          <div className="h-20 w-[270px] max-w-full overflow-hidden">
+          <div className="h-28 w- max-w-full overflow-visible">
             <img
               src={logo.url}
               alt="Moon And Earth Trading And Projects 11cc logo"
